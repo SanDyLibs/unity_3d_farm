d257d0d332e5aa2c274ac2e473aeea64be4feda6
Santoshkumar Vellingiri <42024420+SanDyLibs@users.noreply.github.com>
2024-07-10 17:54:21 +0530

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-DC_3D_Farm-0.1";
+const contentToCache = [
+    "Build/3d_virtual_farm.loader.js",
+    "Build/3d_virtual_farm.framework.js",
+    "Build/3d_virtual_farm.data",
+    "Build/3d_virtual_farm.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
